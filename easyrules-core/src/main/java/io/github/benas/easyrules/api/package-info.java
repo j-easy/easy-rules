2f17415a08106b578e9bbe204da8e22dace1f28f
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-09-25 16:09:54 +0200

    update user name in license header

--- easyrules-core/src/main/java/io/github/benas/easyrules/api/package-info.java
+++ easyrules-core/src/main/java/io/github/benas/easyrules/api/package-info.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- *  Copyright (c) 2014, benas (md.benhassine@gmail.com)
+ *  Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
