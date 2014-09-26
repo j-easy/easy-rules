a456cd4d6f3fd82fb58a065363f12180fac8a5d9
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-09-26 11:42:27 +0200

    add a test suite class for Easy Rules core module

---
+++ easyrules-core/src/test/java/io/github/io/github/benas/easyrules/core/EasyRulesTestSuite.java
@@ -0,0 +1,21 @@
+package io.github.io.github.benas.easyrules.core;
+
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite for Easy Rules core module.
+ *
+ * @author Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {
+        RulePriorityComparisonTest.class,
+        RulePriorityThresholdTest.class,
+        SkipOnFirstAppliedRuleTest.class,
+        JmxRuleRegistrationTest.class})
+public class EasyRulesTestSuite extends TestSuite {
+
+}
