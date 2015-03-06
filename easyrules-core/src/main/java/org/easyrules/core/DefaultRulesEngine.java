c7c485f51c79679f82e02254d8742c81fda07621
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-03-06 20:59:47 +0100

    update license header

--- easyrules-core/src/main/java/org/easyrules/core/DefaultRulesEngine.java
+++ easyrules-core/src/main/java/org/easyrules/core/DefaultRulesEngine.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- *  Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ *  Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
