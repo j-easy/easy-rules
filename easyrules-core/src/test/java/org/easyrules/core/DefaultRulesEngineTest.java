108c7ae03b5e957a5dca1e09a5e410e19a3d9129
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-02-08 00:49:18 +0100

    remove usage of deprecated mockito APIs

--- easyrules-core/src/test/java/org/easyrules/core/DefaultRulesEngineTest.java
+++ easyrules-core/src/test/java/org/easyrules/core/DefaultRulesEngineTest.java
@@ -11,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Map;
 import java.util.Set;
--- easyrules-core/src/test/java/org/easyrules/core/RuleListenerTest.java
+++ easyrules-core/src/test/java/org/easyrules/core/RuleListenerTest.java
@@ -31,7 +31,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
 import static org.mockito.Mockito.*;
