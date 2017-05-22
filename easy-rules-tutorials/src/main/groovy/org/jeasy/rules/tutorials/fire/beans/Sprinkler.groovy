f8487f724247bad8278ec5892cafd5c8ed92d795
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-05-22 23:14:31 +0200

    remove groovy tutorials

--- easy-rules-tutorials/src/main/groovy/org/jeasy/rules/tutorials/fire/beans/Sprinkler.groovy
+++
@@ -1,9 +0,0 @@
-package org.easyrules.samples.fire.beans
-
-@groovy.transform.TupleConstructor
-@groovy.transform.EqualsAndHashCode
-@groovy.transform.ToString(includePackage = false, includeNames = true)
-class Sprinkler {
-    def room;
-    def on = false;
-}
