e7d37053202da96bd5dc151feab5cb334ff37865
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-11-28 11:21:14 +0100

    add javadoc

--- easy-rules-core/src/main/java/org/jeasy/rules/core/InferenceRulesEngine.java
+++ easy-rules-core/src/main/java/org/jeasy/rules/core/InferenceRulesEngine.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License
+ *
+ *  Copyright (c) 2017, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 package org.jeasy.rules.core;
 
 import org.jeasy.rules.api.*;
@@ -23,14 +46,26 @@ public final class InferenceRulesEngine implements RulesEngine {
     private List<RuleListener> ruleListeners;
     private DefaultRulesEngine delegate;
 
+    /**
+     * Create a new inference rules engine with default parameters.
+     */
     public InferenceRulesEngine() {
         this(new RulesEngineParameters());
     }
 
+    /**
+     * Create a new inference rules engine.
+     * @param parameters of the engine
+     */
     public InferenceRulesEngine(RulesEngineParameters parameters) {
         this(parameters, new ArrayList<RuleListener>());
     }
 
+    /**
+     * Create a new inference rules engine.
+     * @param parameters of the engine
+     * @param ruleListeners to apply for each rule
+     */
     public InferenceRulesEngine(RulesEngineParameters parameters, List<RuleListener> ruleListeners) {
         this.parameters = parameters;
         this.ruleListeners = ruleListeners;
