4ba3d2b9d5db4bac85c09beb0febda94cb3047cb
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-12-14 08:29:44 +0100

    use MVEL compiled expressions for better performance

--- easy-rules-mvel/src/main/java/org/jeasy/rules/mvel/MVELAction.java
+++ easy-rules-mvel/src/main/java/org/jeasy/rules/mvel/MVELAction.java
@@ -6,6 +6,8 @@ import org.mvel2.MVEL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
+
 /**
  * This class is an implementation of {@link Action} that uses <a href="https://github.com/mvel/mvel">MVEL</a> to execute the action.
  *
@@ -16,6 +18,7 @@ public class MVELAction implements Action {
     private static Logger LOGGER = LoggerFactory.getLogger(MVELAction.class);
 
     private String expression;
+    private Serializable compiledExpression;
 
     /**
      * Create a new {@link MVELAction}.
@@ -24,12 +27,13 @@ public class MVELAction implements Action {
      */
     public MVELAction(String expression) {
         this.expression = expression;
+        compiledExpression = MVEL.compileExpression(expression);
     }
 
     @Override
     public void execute(Facts facts) {
         try {
-            MVEL.eval(expression, facts.asMap());
+            MVEL.executeExpression(compiledExpression, facts.asMap());
         } catch (Exception e) {
             LOGGER.debug("Unable to evaluate expression: '" + expression + "' on facts: " + facts, e);
         }
--- easy-rules-mvel/src/main/java/org/jeasy/rules/mvel/MVELCondition.java
+++ easy-rules-mvel/src/main/java/org/jeasy/rules/mvel/MVELCondition.java
@@ -6,6 +6,8 @@ import org.mvel2.MVEL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
+
 /**
  * This class is an implementation of {@link Condition} that uses <a href="https://github.com/mvel/mvel">MVEL</a> to evaluate the condition.
  *
@@ -16,6 +18,7 @@ public class MVELCondition implements Condition {
     private static Logger LOGGER = LoggerFactory.getLogger(MVELCondition.class);
 
     private String expression;
+    private Serializable compiledExpression;
 
     /**
      * Create a new {@link MVELCondition}.
@@ -24,12 +27,13 @@ public class MVELCondition implements Condition {
      */
     public MVELCondition(String expression) {
         this.expression = expression;
+        compiledExpression = MVEL.compileExpression(expression);
     }
 
     @Override
     public boolean evaluate(Facts facts) {
         try {
-            return MVEL.evalToBoolean(expression, facts.asMap());
+            return (boolean) MVEL.executeExpression(compiledExpression, facts.asMap());
         } catch (Exception e) {
             LOGGER.debug("Unable to evaluate expression: '" + expression + "' on facts: " + facts, e);
             return false;
