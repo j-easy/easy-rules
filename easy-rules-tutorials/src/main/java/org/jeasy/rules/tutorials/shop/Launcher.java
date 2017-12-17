f2e3ba7493b6f514718c01ec1da321ec283325f6
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-12-17 22:26:42 +0100

    update shop tutorial to use MVEL rules

--- easy-rules-tutorials/src/main/java/org/jeasy/rules/tutorials/shop/Launcher.java
+++ easy-rules-tutorials/src/main/java/org/jeasy/rules/tutorials/shop/Launcher.java
@@ -27,21 +27,35 @@ import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.DefaultRulesEngine;
+import org.jeasy.rules.mvel.MVELRule;
+import org.jeasy.rules.mvel.MVELRuleFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //create a person instance (fact)
         Person tom = new Person("Tom", 14);
         Facts facts = new Facts();
         facts.put("person", tom);
 
-        // create a rules set
-        Rules rules = new Rules();
-        rules.register(new AgeRule());
-        rules.register(new AlcoholRule());
+        // create rules
+        MVELRule ageRule = new MVELRule()
+                .name("age rule")
+                .description("Check if person's age is > 18 and marks the person as adult")
+                .priority(1)
+                .when("person.age > 18")
+                .then("person.setAdult(true);");
+        MVELRule alcoholRule = MVELRuleFactory.createRuleFrom(new File("src/main/java/org/jeasy/rules/tutorials/shop/alcohol-rule.yml"));
 
-        //create a rules engine and fire rules on known facts
+        // create a rule set
+        Rules rules = new Rules();
+        rules.register(ageRule);
+        rules.register(alcoholRule);
+
+        //create a default rules engine and fire rules on known facts
         RulesEngine rulesEngine = new DefaultRulesEngine();
 
         System.out.println("Tom: Hi! can I have some Vodka please?");
