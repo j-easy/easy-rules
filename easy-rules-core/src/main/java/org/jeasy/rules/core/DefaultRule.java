f18389ee9b44b700b37eefa5e98c0d05e74b5f28
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-12-17 22:36:17 +0100

    make DefaultRule extend BasicRule

--- easy-rules-core/src/main/java/org/jeasy/rules/core/DefaultRule.java
+++ easy-rules-core/src/main/java/org/jeasy/rules/core/DefaultRule.java
@@ -26,41 +26,19 @@ package org.jeasy.rules.core;
 import org.jeasy.rules.api.Action;
 import org.jeasy.rules.api.Condition;
 import org.jeasy.rules.api.Facts;
-import org.jeasy.rules.api.Rule;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class DefaultRule implements Rule {
-
-    private String name = Rule.DEFAULT_NAME;
-    private String description = Rule.DEFAULT_DESCRIPTION;
-    private int priority = Rule.DEFAULT_PRIORITY;
+class DefaultRule extends BasicRule {
 
     private Condition condition = Condition.FALSE;
     private List<Action> actions = new ArrayList<>();
 
     DefaultRule(String name, String description, int priority, Condition condition, List<Action> actions) {
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
+        super(name, description, priority);
         this.condition = condition;
         this.actions = actions;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public int getPriority() {
-        return priority;
     }
 
     @Override
@@ -73,41 +51,6 @@ class DefaultRule implements Rule {
         for (Action action : actions) {
             action.execute(facts);
         }
-    }
-
-    /*
-     * Rules are unique according to their names within a rule set (Rules object).
-     */
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        if (!o.getClass().isAssignableFrom(Rule.class)) return false;
-
-        Rule that = (Rule) o;
-
-        return name.equals(that.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return name.hashCode();
-    }
-
-    @Override
-    public int compareTo(final Rule rule) {
-        int priorityComparisonResult = Integer.compare(priority, rule.getPriority());
-        if (priorityComparisonResult != 0) {
-            return priorityComparisonResult;
-        } else {
-            return name.compareTo(rule.getName());
-        }
-    }
-
-    @Override
-    public String toString() {
-        return name;
     }
 
 }
