f066f86c1ae580cdfdba1de180ccd0b9d43a8fe7
Paul Brejla <paul.brejla@googlemail.com>
2018-01-19 11:41:35 +0100

    let reader read MVEL rules from String

--- easy-rules-mvel/src/test/java/org/jeasy/rules/mvel/MVELRuleDefinitionReaderTest.java
+++ easy-rules-mvel/src/test/java/org/jeasy/rules/mvel/MVELRuleDefinitionReaderTest.java
@@ -26,6 +26,8 @@ package org.jeasy.rules.mvel;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,5 +52,23 @@ public class MVELRuleDefinitionReaderTest {
         assertThat(adultRuleDefinition.getCondition()).isEqualTo("person.age > 18");
         assertThat(adultRuleDefinition.getActions()).isEqualTo(Collections.singletonList("person.setAdult(true);"));
     }
+
+    @Test
+    public void testRuleDefinitionReadingFromString() throws Exception {
+        // given
+        String adultRuleDescriptor = new String(Files.readAllBytes(Paths.get("src/test/resources/adult-rule.yml")));
+
+        // when
+        MVELRuleDefinition adultRuleDefinition = ruleDefinitionReader.read(adultRuleDescriptor);
+
+        // then
+        assertThat(adultRuleDefinition).isNotNull();
+        assertThat(adultRuleDefinition.getName()).isEqualTo("adult rule");
+        assertThat(adultRuleDefinition.getDescription()).isEqualTo("when age is greater then 18, then mark as adult");
+        assertThat(adultRuleDefinition.getPriority()).isEqualTo(1);
+        assertThat(adultRuleDefinition.getCondition()).isEqualTo("person.age > 18");
+        assertThat(adultRuleDefinition.getActions()).isEqualTo(Collections.singletonList("person.setAdult(true);"));
+    }
+
 
 }
