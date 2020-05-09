acd9a8ffa0f1febdad74ae0a95fd45aa39fe8f64
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2020-05-09 23:51:57 +0200

    Remove unused import

--- easy-rules-tutorials/src/main/java/org/jeasy/rules/tutorials/web/IndexServlet.java
+++ easy-rules-tutorials/src/main/java/org/jeasy/rules/tutorials/web/IndexServlet.java
@@ -25,7 +25,6 @@ package org.jeasy.rules.tutorials.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
