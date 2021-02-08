ab75210854e06dff39469bd0a42b8c45ebf826ed
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2021-02-08 22:03:08 +0100

    Update year in license header

--- easy-rules-archetype/src/main/resources/archetype-resources/src/main/java/Launcher.java
+++ easy-rules-archetype/src/main/resources/archetype-resources/src/main/java/Launcher.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License
  *
- *  Copyright (c) 2020, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *  Copyright (c) 2021, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
