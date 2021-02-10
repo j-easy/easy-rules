c04898bae606650a85a43c29071e6e3183290d6b
aston <jianzemail@163.com>
2021-02-10 21:43:35 +0800

    remove modified code

--- easy-rules-core/src/main/java/org/jeasy/rules/core/DefaultRulesEngine.java
+++ easy-rules-core/src/main/java/org/jeasy/rules/core/DefaultRulesEngine.java
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Default {@link RulesEngine} implementation.
@@ -150,12 +149,6 @@ public final class DefaultRulesEngine extends AbstractRulesEngine {
 
     private void log(Facts facts) {
         LOGGER.debug("Known facts:");
-
-        if (Objects.isNull(facts)){
-            LOGGER.debug("facts is null");
-            return;
-        }
-
         for (Fact<?> fact : facts) {
             LOGGER.debug("{}", fact);
         }
