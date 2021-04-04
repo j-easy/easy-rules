69085d54f68c7ac872d9efcad704c4fb1436ee39
Abdelgadir.Ibrahim <abdelgadir.ibrahim@freedom-finance.com>
2021-04-04 19:53:02 +0100

    build tidy up. added lombok. lombok(ation)

--- easy-rules-core/src/main/java/org/jeasy/rules/core/ActionMethodOrderBean.java
+++ easy-rules-core/src/main/java/org/jeasy/rules/core/ActionMethodOrderBean.java
@@ -21,60 +21,39 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
+
 package org.jeasy.rules.core;
 
 import java.lang.reflect.Method;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 /**
  * Utility class that associates an action method and its execution order.
  *
  * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  */
+@EqualsAndHashCode
+@Getter
 class ActionMethodOrderBean implements Comparable<ActionMethodOrderBean> {
 
-    private final Method method;
-    private final int order;
+  private final Method method;
+  private final int order;
 
-    ActionMethodOrderBean(final Method method, final int order) {
-        this.method = method;
-        this.order = order;
+  ActionMethodOrderBean(final Method method, final int order) {
+    this.method = method;
+    this.order = order;
+  }
+
+  @Override
+  public int compareTo(final ActionMethodOrderBean actionMethodOrderBean) {
+    if (order < actionMethodOrderBean.getOrder()) {
+      return -1;
+    } else if (order > actionMethodOrderBean.getOrder()) {
+      return 1;
+    } else {
+      return method.equals(actionMethodOrderBean.getMethod()) ? 0 : 1;
     }
-
-    public int getOrder() {
-        return order;
-    }
-
-    public Method getMethod() {
-        return method;
-    }
-
-    @Override
-    public int compareTo(final ActionMethodOrderBean actionMethodOrderBean) {
-        if (order < actionMethodOrderBean.getOrder()) {
-            return -1;
-        } else if (order > actionMethodOrderBean.getOrder()) {
-            return 1;
-        } else {
-            return method.equals(actionMethodOrderBean.getMethod()) ? 0 : 1;
-        }
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ActionMethodOrderBean)) return false;
-
-        ActionMethodOrderBean that = (ActionMethodOrderBean) o;
-
-        if (order != that.order) return false;
-        return method.equals(that.method);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = method.hashCode();
-        result = 31 * result + order;
-        return result;
-    }
+  }
 
 }
--- easy-rules-support/src/main/java/org/jeasy/rules/support/RuleDefinition.java
+++ easy-rules-support/src/main/java/org/jeasy/rules/support/RuleDefinition.java
@@ -21,89 +21,38 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
-package org.jeasy.rules.support;
 
-import org.jeasy.rules.api.Rule;
+package org.jeasy.rules.support;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.jeasy.rules.api.Rule;
 
 /**
- * Rule definition as defined in a rule descriptor.
- * This class encapsulates the static definition of a {@link Rule}.
- *
- * Rule definitions are produced by a {@code RuleDefinitionReader}s
- * and consumed by rule factories to create rules.
+ * Rule definition as defined in a rule descriptor. This class encapsulates the static definition of
+ * a {@link Rule}.
+ * <p>
+ * Rule definitions are produced by a {@code RuleDefinitionReader}s and consumed by rule factories
+ * to create rules.
  *
  * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  */
+@Getter
+@Setter
 public class RuleDefinition {
 
-    private String name = Rule.DEFAULT_NAME;
-    private String description = Rule.DEFAULT_DESCRIPTION;
-    private int priority = Rule.DEFAULT_PRIORITY;
-    private String condition;
-    private List<String> actions = new ArrayList<>();
-    private List<RuleDefinition> composingRules = new ArrayList<>();
-    private String compositeRuleType;
+  private String name = Rule.DEFAULT_NAME;
+  private String description = Rule.DEFAULT_DESCRIPTION;
+  private int priority = Rule.DEFAULT_PRIORITY;
+  private String condition;
+  private List<String> actions = new ArrayList<>();
+  private List<RuleDefinition> composingRules = new ArrayList<>();
+  private String compositeRuleType;
 
-    public String getName() {
-        return name;
-    }
+  public boolean isCompositeRule() {
+    return !composingRules.isEmpty();
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public List<String> getActions() {
-        return actions;
-    }
-
-    public void setActions(List<String> actions) {
-        this.actions = actions;
-    }
-
-    public void setComposingRules(List<RuleDefinition> composingRuleDefinitions) {
-        this.composingRules = composingRuleDefinitions;
-    }
-
-    public void setCompositeRuleType(String compositeRuleType) {
-        this.compositeRuleType = compositeRuleType;
-    }
-
-    public String getCompositeRuleType() {
-        return compositeRuleType;
-    }
-
-    public List<RuleDefinition> getComposingRules() {
-        return composingRules;
-    }
-
-    public boolean isCompositeRule() {
-        return !composingRules.isEmpty();
-    }
 }
